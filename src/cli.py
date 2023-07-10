7cf65290e641f8fdfb0384bafd04283b6d95588e
Ada <the.nostra.tymus@gmail.com>
2023-07-09 22:32:01 -0400

    added the noise to the CLI and created the requirements file

--- src/cli.py
+++ src/cli.py
@@ -14,16 +14,16 @@ def verify_directory(directory, clear):
 
 
 def main(flags):
-    max_signals = flags['max_signals']
-    image_size = (flags['image_height'], flags['image_width'])
-    directory = flags['generation_directory']
+    max_signals = flags.max_signals
+    image_size = flags.image_height, flags.image_width
+    directory = flags.generation_directory
 
-    verify_directory(directory, flags['clear_generation_directory'])
+    verify_directory(directory, flags.clear_generation_directory)
 
-    for _ in range(flags['quantity']):
+    for _ in range(flags.quantity):
         sigs = create_signals(max_signals=max_signals)
 
-        spectrogram = create_spectrogram(sigs + 1.15 * np.random.randn(len(sigs)), 2 ** 11)
+        spectrogram = create_spectrogram(sigs + flags.noise_intensity * np.random.randn(len(sigs)), 2 ** 11)
         spectrogram = np.hsplit(spectrogram, 2)[0]
 
         spectrogram = ((spectrogram - spectrogram.min()) * (
@@ -31,23 +31,25 @@ def main(flags):
 
         annotate(spectrogram, image_size, directory)
 
+        print(f"{_} of {flags.quantity}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--max_signals', type=int,
+    parser.add_argument('--max-signals', type=int,
                         default=40,
                         help='The number of signals to generate in each spectrogram')
 
-    parser.add_argument('--image_height', type=int,
+    parser.add_argument('--image-height', type=int,
                         default=1500,
                         help='The height of the generated images')
 
-    parser.add_argument('--image_width', type=int,
+    parser.add_argument('--image-width', type=int,
                         default=600,
                         help='The width of the generated images')
 
-    parser.add_argument('--generation_directory', type=str,
+    parser.add_argument('--generation-directory', type=str,
                         default="../data_gen/",
                         help='The directory to store the data in')
 
@@ -55,9 +57,13 @@ if __name__ == "__main__":
                         default=200,
                         help='The number of examples to generate')
 
-    parser.add_argument('--clear_generation_directory', type=bool,
+    parser.add_argument('--clear-generation-directory', type=bool,
                         default=True,
-                        help='Boolean to indicate whether to clear the genration directory before use.')
+                        help='Boolean to indicate whether to clear the generation directory before use.')
+
+    parser.add_argument('--noise-intensity', type=float,
+                        default=0.999,
+                        help='The scalar to multiply the noise by')
 
     parsed_flags, _ = parser.parse_known_args()
 
